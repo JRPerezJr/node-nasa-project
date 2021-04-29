6b623f92e70d2fa2f35250002101854168dcccc0
Juan <pront23@gmail.com>
2021-04-29 14:12:09 +0900

    new function addNewLaunch in launches route

--- server/src/models/launches.models.js
+++ server/src/models/launches.models.js
@@ -1,12 +1,14 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
   launchData: new Date('December 27, 2030'),
   destination: 'Kepler-442 b',
-  customer: ['ZTM', 'NASA'],
+  customers: ['Zero To Mastery', 'NASA'],
   upcoming: true,
   success: true,
 };
@@ -17,4 +19,17 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
-module.exports = { getAllLaunches };
+function addNewLaunch(launch) {
+  latestFlightNumber += 1;
+  launches.set(
+    launch.flightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ['Zero To Mastery', 'NASA'],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
+module.exports = { getAllLaunches, addNewLaunch };
